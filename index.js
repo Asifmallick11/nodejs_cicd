7883c7afc8dd629fd02fbef0e4e7c899bb20b060
Asifmallick11 <asifsmallick@gmail.com>
2025-07-22 21:06:48 +0530

    updated index.js with a health route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/" , (req , res) => {
     res.send('<h1>Hello from your simple Express server!</h1>')
 })
 
+app.get('/health', (req, res) => {
+      res.status(200).json({ status: 'UP' });
+    });
+
 app.listen(3000 , () => {
     console.log("connected to server")
 })
